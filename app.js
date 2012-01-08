ea338645d18eaf23b91730713aa0a8bca978a009
nns <nozaky2010@gmail.com>
2012-01-08 05:30:48 +0000

    s

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.get('/logout',function(req, res){
 	delete req.session.controller;
 	res.redirect('/');
 });
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 
